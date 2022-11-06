c491ff346c8610e43bbcebc377ce3690f1f33714
DRAFmatsutake <DRAFmatsutake@gmail.com>
2022-11-06 13:58:56 +0900

    受信処理の実装

--- source/source.ino
+++ source/source.ino
@@ -8,28 +8,49 @@
 #include <WiFi.h>
 #include <WebServer.h>
 
-#include "Config.h" // definition 'ssid' and 'password'
+#include "Config.h" // 'ssid' と 'password'　の定義
 
-//赤外線センサ
+//赤外線センサ ライブラリ設定
 #define IR_SEND_PIN     4   //D4
-#define IR_RECEIVE_PIN 21   //D5
+#define IR_RECEIVE_PIN 21   //D21
+#define RAW_BUFFER_LENGTH 400
+#define RECORD_GAP_MICROS 12000
+//#define RAW_BUFFER_LENGTH  n
+#define DECODE_DISTANCE     // universal decoder for pulse width or pulse distance protocols
+//#define DECODE_HASH         // special decoder for all protocols
 #include <IRremote.hpp>
 
+//ピン設定
 #define SELETMODE_PIN   5  //pull up
 
-WebServer server(80);
+
+//プロトタイプ宣言
 void Index(void);
 String SelectSignal(String key,String html);
-char prontoData[] = SIGNAL_A;
-char recData[1024];
+void IRrecProc();
+
+//変数定義
+String recSignal="";
+String sendSignal="";
+char servermode;
+char sendFlug=false;
+
+//webサーバー初期化
+WebServer server(80);
+
 //----------------------------------------------------------
 //           esp32 set up and main prosess
 //----------------------------------------------------------
 
 
+//+===========================================
 void setup() {
-  pinMode(PIN_0,OUTPUT);
-  Serial.begin(9600);
+  //Pin設定
+  pinMode(SELETMODE_PIN,INPUT);
+  
+  Serial.begin(9600);         //シリアル通信　初期化
+  
+  //Wifi　初期化＆接続
   WiFi.begin(ssid, password); 
   Serial.println("\nConnecing...");   
   while (WiFi.status() != WL_CONNECTED) {
@@ -39,14 +60,28 @@ void setup() {
   Serial.println("WiFi Connected.");
   Serial.print("IP = ");
   Serial.println(WiFi.localIP());
+
+  //webサーバーの開始
   server.on("/",Index);
   server.begin();
   IrSender.begin();
+  IrReceiver.begin(IR_RECEIVE_PIN);
 }
 
+//+===========================================
 void loop() {
-  // put your main code here, to run repeatedly:
+    servermode = digitalRead(SELETMODE_PIN);    //モード状態の取得
+    if(servermode == 0){
+      IRrecProc();
+    }
+    if(sendFlug==true){
+      sendFlug=false;
+      IrSender.sendPronto(sendSignal.c_str());
+      delay(2000);  
+    }
+     
     server.handleClient();
+
 }
 
 
@@ -60,19 +95,30 @@ void Index(void){
     html += "<meta charset=\"UTF-8\">";
     html += "<title>リモコン</title>";
     html += "</head>";
-    
     html += "<body>";
-    if (server.method()==HTTP_POST){
-      html += SelectSignal(server.arg("switch"));
+    
+    //ここからモードによって分岐する
+    if(servermode == 1){    //送信モード
+        html += "<h1>IRserver MODE</h1>";
+        if (server.method()==HTTP_POST){
+          html += SelectSignal(server.arg("switch"));
+         }
+        html += "<form action=\"./\" method=\"POST\">";
+        html += "<input name=\"switch\" type=\"text\">";
+        html += "<input type=\"submit\" value=\"送信\">";
+        html += "</form>";
      }
-    html += "<form action=\"./\" method=\"POST\">";
-    html += "<input name=\"switch\" type=\"text\">";
-    html += "<input type=\"submit\" value=\"送信\">";
-    html += "</form>";
-    html += "</body></html>";
-
-    // HTMLを出力する
-    server.send(200, "text/html", html);
+     else{                  //受信モード
+        html += "<h1>IRreceiver MODE</h1>";
+        html += "<h2>受信信号</h2>";
+        if(recSignal=="")
+          html += "<div>No signal</div>";
+        else
+          html += "<div>"+recSignal+"</div>";
+     }
+      
+     html += "</body></html>";
+    server.send(200, "text/html", html);    // HTMLを出力する
  }
 
 
@@ -81,17 +127,27 @@ void Index(void){
 //                  signal prosess
 //----------------------------------------------------------
 String SelectSignal(String key){
+  sendFlug=true;
   String result;
   if(key=="light_on")
-    {result="light_on";digitalWrite(PIN_0,1);}
+    {result="light_on";}
   else if(key=="light_off")
-    {result="light_off";digitalWrite(PIN_0,0);}
+    {result="light_off";}
   else
-    {result="\""+key+"\" is invalid word";
-    //IrSender.sendPronto(key.c_str());
-    IrSender.sendPronto(prontoData);
-    Serial.println("send trun on at " +  String(millis()));
-    delay(2000);
+    {
+      result="\""+key+"\" is invalid word";
+      sendSignal=key;
     }
   return result;
+}
+
+//+===========================================
+void IRrecProc(){
+    if (IrReceiver.decode()) {
+      recSignal="";
+      IrReceiver.compensateAndStorePronto(&recSignal);
+      Serial.print("rec signal is : ");
+      Serial.println(recSignal);  
+      IrReceiver.resume(); // Enable receiving of the next value
+  }
 }
